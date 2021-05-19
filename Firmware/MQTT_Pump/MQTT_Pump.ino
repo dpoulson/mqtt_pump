5264fe1c69ab2ec906b6a1ab58e6b9024e7825c4
dpoulson <darren.poulson@gmail.com>
2021-05-19 01:27:03 +0100

    First pass at arduino code using Homie standard

--- Firmware/MQTT_Pump/MQTT_Pump.ino
+++ Firmware/MQTT_Pump/MQTT_Pump.ino
@@ -1,122 +1,196 @@
+/*
+    This sketch establishes an MQTT connection to your broker and publishes a device with several dummy nodes and properties.
+*/
+
+#if defined(ARDUINO_ARCH_ESP8266)
 #include <ESP8266WiFi.h>
-#include <Ticker.h>
-#include <AsyncMqttClient.h>
+#else
+#include "WiFi.h"
+#endif
+#include <LeifHomieLib.h>
+#include "config.h"
 
-#define WIFI_SSID "My_Wi-Fi"
-#define WIFI_PASSWORD "my-awesome-password"
+HomieDevice homie;
 
-#define MQTT_HOST IPAddress(192, 168, 1, 10)
-#define MQTT_PORT 1883
+const char* ssid     = STASSID;
+const char* password = STAPSK;
 
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+#define PUMP_OUT D1
+#define FLOAT_HIGH D2
+#define FLOAT_LOW D3
+#define MANUAL_PUMP D4
 
-WiFiEventHandler wifiConnectHandler;
-WiFiEventHandler wifiDisconnectHandler;
-Ticker wifiReconnectTimer;
-
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
-
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  connectToMqtt();
-}
-
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-  wifiReconnectTimer.once(2, connectToWifi);
-}
-
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
-
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-  mqttClient.publish("test/lol", 0, true, "test 1");
-  Serial.println("Publishing at QoS 0");
-  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
-  Serial.print("Publishing at QoS 1, packetId: ");
-  Serial.println(packetIdPub1);
-  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
-  Serial.print("Publishing at QoS 2, packetId: ");
-  Serial.println(packetIdPub2);
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
+// We'll need a place to save pointers to our created properties so that we can access them again later.
+HomieProperty * pPropPump=NULL;
+HomieProperty * pPropManual=NULL;
+HomieProperty * pPropFloatHigh=NULL;
+HomieProperty * pPropFloatLow=NULL;
 
 void setup() {
   Serial.begin(115200);
+
+  // We start by connecting to a WiFi network
+
+  pinMode(PUMP_OUT, OUTPUT);
+  pinMode(FLOAT_HIGH, INPUT);
+  pinMode(FLOAT_LOW, INPUT);
+  pinMode(MANUAL_PUMP, INPUT);
+
   Serial.println();
   Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  connectToWifi();
+  HomieLibRegisterDebugPrintCallback([](const char * szText){
+    Serial.printf("%s",szText);
+    });
+
+
+
+  {
+
+
+    HomieNode * pNode=homie.NewNode();
+
+    pNode->strID="properties";
+    pNode->strFriendlyName="Properties";
+//    pNode->strType="customtype";
+
+    HomieProperty * pProp;
+
+
+    pPropPump=pProp=pNode->NewProperty();
+    pProp->strFriendlyName="Pump";
+    pProp->strID="pump";
+    pProp->strFormat="";
+    pProp->SetRetained(false);
+    pProp->datatype=homieEnum;
+    pProp->SetSettable(true);
+    pProp->strFormat="ON,OFF";
+    pProp->AddCallback([](HomieProperty * pSource) {
+      //this property is settable. We'll print it into the console whenever it's updated.
+      //you can set it from MQTT Explorer by publishing a number between 0-100 to homie/examplehomiedev/nodeid1/dimmer
+      //but remember to check the *retain* box.
+      Serial.printf("%s is now %s\n",pSource->strFriendlyName.c_str(),pSource->GetValue().c_str());
+    });
+    
+
+    pPropFloatHigh=pProp=pNode->NewProperty();
+    pProp->strFriendlyName="Float High";
+    pProp->strID="floathigh";
+    pProp->strFormat="";
+    pProp->SetRetained(true);
+    pProp->SetBool(false);
+    pProp->datatype=homieBool;
+
+    pPropFloatLow=pProp=pNode->NewProperty();
+    pProp->strFriendlyName="Float Low";
+    pProp->strID="floatlow";
+    pProp->strFormat="";
+    pProp->SetRetained(true);
+    pProp->SetBool(false);
+    pProp->datatype=homieBool;
+
+
+    pPropPump=pProp=pNode->NewProperty();
+    pProp->strFriendlyName="Manual";
+    pProp->strID="manual";
+    pProp->strFormat="";
+    pProp->SetRetained(false);
+    pProp->datatype=homieEnum;
+    pProp->strFormat="ON,OFF";
+
+  }
+
+  homie.strFriendlyName="Garage Pump";
+  homie.strID="garagepump";
+  homie.strID.toLowerCase();
+
+  homie.strMqttServerIP=MQTT_IP;
+  homie.strMqttUserName=MQTT_USER;
+  homie.strMqttPassword=MQTT_PASS;
+
+  homie.Init();
 }
 
-void loop() {
+unsigned long lastRun = 0;        // When was the pump last turned on
+unsigned long manualPressed = 0;  // When was the manual button pressed
+int coolDown = 1000*60*10;        // Only let the pump run once every 10 minutes
+int maxRun = 1000*60*5;           // Run for a max of 5 minutes
+
+int high_input = 0;
+int low_input = 0;
+int manual_input = 0;
+int pump_state = 0;
+int last_pump_state = 0;
+
+void loop()
+{
+  homie.Loop();
+
+  high_input = digitalRead(FLOAT_HIGH);
+  low_input = digitalRead(FLOAT_LOW);
+  manual_input = digitalRead(MANUAL_PUMP);
+
+  // If high level float is active, and its more than 'coolDown' since
+  // the last run, then set the pump to on
+  if(high_input == 1 && millis() - lastRun > coolDown) 
+  {
+    if (pump_state == 0) {  // Only do stuff when state changes
+      lastRun = millis();
+      pump_state = 1;
+      if(homie.IsConnected()) {
+        if(pPropPump) pPropPump->SetValue("ON");
+        if(pPropFloatHigh) pPropFloatHigh->SetBool(true);
+        if(pPropFloatLow) pPropFloatLow->SetBool(true);
+      }
+    }
+  }
+
+  // If its been running for  'maxRun', then turn the pump off.
+  if(millis() - lastRun > maxRun) 
+  {
+    pump_state = 0;
+  }
+
+  // If the lower float switch is off, then the motor should be off.
+  if(low_input == 0) 
+  {
+    if(pump_state == 1)
+    {
+      if(homie.IsConnected()) {
+        if(pPropFloatLow) pPropFloatLow->SetBool(false);
+        if(pPropFloatLow) pPropFloatHigh->SetBool(false);
+      }
+    }
+    pump_state = 0;
+  }
+
+  // Turn the pump on or off
+  if(pump_state == 1)
+  {
+    digitalWrite(PUMP_OUT, HIGH);   
+  } else {
+    digitalWrite(PUMP_OUT, LOW);
+    if(homie.IsConnected()) {
+        if(pPropPump) pPropPump->SetValue("OFF");
+        if(pPropFloatHigh) pPropFloatHigh->SetBool(false);
+        if(pPropFloatLow) pPropFloatLow->SetBool(false);
+      }
+  }
 }
